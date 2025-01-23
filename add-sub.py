8443043b6f13b094796bcb671c4e3a982704c2af
MohammadReza Omrani <52138433+mohammad-reza-omrani@users.noreply.github.com>
2025-01-23 15:43:56 -0600

    Add files via upload

---
+++ add-sub.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arrayOne = np.array([[1, 2], [3, 4]])
+arrayTwo = np.array([[5, 6], [7, 8]])
+
+add_result = arrayOne + arrayTwo
+sub_result = arrayOne - arryayTwo
+
+print("Matrix Addition:")
+print(arrayOne)
+print("Matrix Subtraction:")
+print(arrayTwo)
